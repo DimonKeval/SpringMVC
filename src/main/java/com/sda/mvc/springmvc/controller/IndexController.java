e76171561123d7836f838a8bffa2802474338b2a
Dimon_Keval <kevaldima@gmail.com>
2020-10-24 10:54:29 +0200

    Added security and login page commit

---
+++ src/main/java/com/sda/mvc/springmvc/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.sda.mvc.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String test(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+}
